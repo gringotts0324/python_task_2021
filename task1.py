c74b0dd4dc5e1e2addbd093019098099b12a09f4
kei <gringotts0324@gmail.com>
2021-09-10 00:07:49 +0900

    need review

--- task1.py
+++ task1.py
@@ -1,14 +1,27 @@
-#task1-1, 1-2
-source = []
+import pandas as pd
+data = pd.read_csv("kimetsu.csv")
+list = data["名前"].tolist()
 
-name = input("キーワードを入力してください")
+source = ["ねずこ", "たんじろう", "きょうじゅろう", "ぎゆう", "げんや", "かなお", "ぜんいつ", "むいちろう"]
 
-if name in source:
-    print(f"{name}はリストに存在します")
-else:
-    print(f"{name}はリストに存在しません")
-    source.append(name)
+for name in list:
+    if name in source:
+        pass
+    else:
+        source.append(name)
+
+
+def search():
+    word = input("鬼滅の登場人物の名前を入力してください")
+    if word in source:
+        print(f"{word}が見つかりました")
+    else:
+        print(f"{word}はリストに存在しません")
+        source.append(word)
     
+search()
 
-
-
+df = pd.DataFrame(source)
+df.rename(columns = {0: "名前"}, inplace=True)
+print(df)
+df.to_csv("kimetsu2.csv")
