62989e26f4ca311fb66cf35a28fc73280bd4eb45
kei <gringotts0324@gmail.com>
2021-09-14 16:27:30 +0900

    revision3

--- task1.py
+++ task1.py
@@ -4,16 +4,11 @@ def search():
     
     #csvを読み込んで、カラム名が「名前」のカラムをリスト化
     data = pd.read_csv("kimetsu.csv")
+    source = []
     list = data["名前"].tolist()
-    source = ["ねずこ", "たんじろう", "きょうじゅろう", "ぎゆう", "げんや", "かなお", "ぜんいつ", "むいちろう"]
-
-    #csvから読み込んだ名前がソースリストにない場合のみ、追加
-    for name in list:
-        if name in source:
-            pass
-        else:
-            source.append(name)
+    source.extend(list)
     
+    #入力し、リストに追加
     word = input("鬼滅の登場人物の名前を入力してください")
     if word in source:
         print(f"{word}が見つかりました")
