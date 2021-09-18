dad3c0eb5eee1305e39466404ec41d169e1c68ba
kei <gringotts0324@gmail.com>
2021-09-18 23:03:44 +0900

    first-commit

--- task2-2.py
+++ task2-2.py
@@ -3,13 +3,18 @@ from selenium.webdriver import Chrome, ChromeOptions
 from selenium.webdriver import Firefox
 from selenium.webdriver.firefox.options import Options
 import time
+import datetime
 import pandas as pd
 from webdriver_manager.chrome import ChromeDriverManager
+
+#変数
+LOG_FILE_PATH = "./log/log_{datatime}.log"
+log_file_path = LOG_FILE_PATH.
 
 # Chromeを起動する関数
 def set_driver(driver_path, headless_flg):
     if "chrome" in driver_path:
-          options = ChromeOptions()
+        options = ChromeOptions()
     else:
       options = Options()
 
@@ -32,11 +37,20 @@ def set_driver(driver_path, headless_flg):
     else:
         return Firefox(executable_path=os.getcwd()  + "/" + driver_path,options=options)
 
+# ログに関する処理
+def log(txt):
+    now = datetime.datetime.now().strftime("%Y/%m/%d %H:%M:%S")
+    with open(log_file_path, "a", encoding = "utf-8_sig") as file:
+        file.write(f"{now}+{txt}")
+
+
 # main処理
-
-
 def main():
+    log("処理開始")
     search_keyword = "高収入"
+    #search_keyword = input("検索キーワードを入力してください")
+    log(f"検索ワードは、{search_keyword}です")
+
     # driverを起動
     if os.name == 'nt': #Windows
         driver = set_driver("chromedriver.exe", False)
@@ -57,27 +71,67 @@ def main():
     driver.find_element_by_class_name("topSearch__text").send_keys(search_keyword)
     # 検索ボタンクリック
     driver.find_element_by_class_name("topSearch__button").click()
+    #driver.implicitly_wait(10)
+
+    #URLを用いる場合
+    #driver.get(f"https://tenshoku.mynavi.jp/list/kw{search_keyword}/?jobsearchType=14&searchType=18")
 
     # ページ終了まで繰り返し取得
     # 検索結果の一番上の会社名を取得
-    name_list = driver.find_elements_by_class_name("cassetteRecruit__name")
-    
+    #name_list = driver.find_elements_by_class_name("cassetteRecruit__name")
+        
     # 空のDataFrame作成
     df = pd.DataFrame()
 
     # 1ページ分繰り返し
-    print(len(name_list))
-    for name in name_list:
-        print(name.text)
-        # DataFrameに対して辞書形式でデータを追加する
-        df = df.append(
-            {"会社名": name.text, 
-             "項目B": "",
-             "項目C": ""}, 
-            ignore_index=True)
-        
-    print(df)
+    #print(len(name_list))
+    while True:
+        elements = driver.find_elements_by_class_name("cassetteRecruit__content")
+        for element in elements:
+            # DataFrameに対して辞書形式でデータを追加する
+            name = element.find_element_by_class_name("cassetteRecruit__name")
+            title = element.find_element_by_class_name("cassetteRecruit__copy")
+            df = df.append(
+                {"会社名": name.text, 
+                "求人タイトル": title.text}, 
+                ignore_index=True)
+            log(f"{name}を追加")
+        #次ページ
+        try:
+            next_page = driver.find_element_by_class_name("iconFont--arrowLeft")
+            next_page_url = next_page.get_attribute("href")
+            driver.get(next_page_url)
+        except:
+            print("最後のページです")
+            break #ループから抜ける
 
+    """
+    #loop
+    pages = driver.find_element_by_class_name("pager_text").text()
+    page_loop = int(pages) - 1
+
+    for n in range(page_loop):
+        elements = driver.find_elements_by_class_name("cassetteRecruit__content")
+        for element in elements:
+            # DataFrameに対して辞書形式でデータを追加する
+            name = element.find_element_by_class_name("cassetteRecruit__name")
+            title = element.find_element_by_class_name("cassetteRecruit__copy")
+            df = df.append(
+                {"会社名": name.text, 
+                "項目B": title.text,
+                "項目C": ""}, 
+                ignore_index=True)
+        #次ページ
+        try:
+            #driver.find_element_by_class_name("kei").click()
+            driver.find_element_by_class_name("iconFont--arrowLeft").click()
+        except:
+            print("最後のページです")
+            break #ループから抜ける
+    """
+
+    print(df)
+    #df.to_csv("task2_results.csv", encoding = "utf-8-sig")
 
 # 直接起動された場合はmain()を起動(モジュールとして呼び出された場合は起動しないようにするため)
 if __name__ == "__main__":
