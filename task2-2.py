874efddf9095849e47f4bf2043615c596482c516
kei <gringotts0324@gmail.com>
2021-09-28 23:35:56 +0900

    revision

--- task2-2.py
+++ task2-2.py
@@ -7,9 +7,11 @@ import datetime
 import pandas as pd
 from webdriver_manager.chrome import ChromeDriverManager
 
-#変数
-LOG_FILE_PATH = "./log/log_{datatime}.log"
-log_file_path = LOG_FILE_PATH.
+#ログファイルのパスとファイル名
+LOG_FILE_PATH = "./log/log_{datetime}.log" 
+EXP_CSV_PATH = "task2_list_{search_keyword}_{datetime}.csv"
+logtime = datetime.datetime.now().strftime("%y_%m_%d_%H_%M_%S")
+log_file_path = LOG_FILE_PATH.format(datetime=logtime) #変数LOG_FILE_PATHの{datetime}にlogtimeを代入
 
 # Chromeを起動する関数
 def set_driver(driver_path, headless_flg):
@@ -40,8 +42,10 @@ def set_driver(driver_path, headless_flg):
 # ログに関する処理
 def log(txt):
     now = datetime.datetime.now().strftime("%Y/%m/%d %H:%M:%S")
+    logtxt = f"{now},{txt}"
     with open(log_file_path, "a", encoding = "utf-8_sig") as file:
-        file.write(f"{now}+{txt}")
+        file.write(logtxt + "\n")
+    print(logtxt)
 
 
 # main処理
@@ -83,7 +87,11 @@ def main():
     # 空のDataFrame作成
     df = pd.DataFrame()
 
-    # 1ページ分繰り返し
+    #ログ用のカウント
+    count = 0
+    success = 0
+    fail = 0
+
     #print(len(name_list))
     while True:
         elements = driver.find_elements_by_class_name("cassetteRecruit__content")
@@ -95,43 +103,25 @@ def main():
                 {"会社名": name.text, 
                 "求人タイトル": title.text}, 
                 ignore_index=True)
-            log(f"{name}を追加")
-        #次ページ
-        try:
+            log(f"{count}件目追加成功：{name.text}")
+            count+=1
+        #次ページ 
+        next_page_count = driver.find_elements_by_class_name("iconFont--arrowLeft")
+        if len(next_page_count) > 10:
             next_page = driver.find_element_by_class_name("iconFont--arrowLeft")
             next_page_url = next_page.get_attribute("href")
             driver.get(next_page_url)
-        except:
-            print("最後のページです")
+        else:
+            print("最後のページです。終了します")
             break #ループから抜ける
 
-    """
-    #loop
-    pages = driver.find_element_by_class_name("pager_text").text()
-    page_loop = int(pages) - 1
-
-    for n in range(page_loop):
-        elements = driver.find_elements_by_class_name("cassetteRecruit__content")
-        for element in elements:
-            # DataFrameに対して辞書形式でデータを追加する
-            name = element.find_element_by_class_name("cassetteRecruit__name")
-            title = element.find_element_by_class_name("cassetteRecruit__copy")
-            df = df.append(
-                {"会社名": name.text, 
-                "項目B": title.text,
-                "項目C": ""}, 
-                ignore_index=True)
-        #次ページ
-        try:
-            #driver.find_element_by_class_name("kei").click()
-            driver.find_element_by_class_name("iconFont--arrowLeft").click()
-        except:
-            print("最後のページです")
-            break #ループから抜ける
-    """
-
+    #csv出力
     print(df)
-    #df.to_csv("task2_results.csv", encoding = "utf-8-sig")
+    df.to_csv("task2_list.csv")
+    exp_time = datetime.datetime.now().strftime("%Y-%m-%d_%H:%M:%S")
+    df.to_csv(EXP_CSV_PATH.format(search_keyword=search_keyword, datetime=exp_time), encoding = "utf-8-sig")
+    log(f"処理完了")
+
 
 # 直接起動された場合はmain()を起動(モジュールとして呼び出された場合は起動しないようにするため)
 if __name__ == "__main__":
